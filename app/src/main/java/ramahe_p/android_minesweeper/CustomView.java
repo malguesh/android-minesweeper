0c670af87bd4f67aa1c1ea0ae18588176a3472b6
Philippine Ramaherison <philippine.ramaherison@gmail.com>
2016-04-04 15:39:09 +0100

    Milestone 02:
    - Add onMeasure method in CustomView class
       - Call to setMeasureDimension

--- app/src/main/java/ramahe_p/android_minesweeper/CustomView.java
+++ app/src/main/java/ramahe_p/android_minesweeper/CustomView.java
@@ -1,7 +1,9 @@
 package ramahe_p.android_minesweeper;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class CustomView extends View {
@@ -23,5 +25,21 @@ public class CustomView extends View {
 
     private void init() {
 
+    }
+
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        // TODO: fill the method to change the customview as a square
+        // TODO: call the setMeasureDimension(width, height)
     }
 }
